db7a38bb2188733ad79503ef7c2860d4edc1aacf
Maksim-dew <maksim.medvedev0307@mail.ru>
2022-12-13 23:28:08 +0300

    Добавьте файлы проекта.

---
+++ Server/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ClientServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener serverSocket = new TcpListener(IPAddress.Any, 7000);
+                Console.WriteLine("Server started");
+                serverSocket.Start();
+
+                while (true)
+                {
+                    TcpClient clientSocket = serverSocket.AcceptTcpClient();
+                    NetworkStream stream = clientSocket.GetStream();
+
+                    byte[] bytes = new byte[256];
+                    int length = stream.Read(bytes, 0, bytes.Length);
+                    string request = Encoding.ASCII.GetString(bytes, 0, length);
+                    Console.WriteLine("Got request: " + request);
+
+                    string message = "Length of yuor request: " + request.Length;
+                    bytes = Encoding.ASCII.GetBytes(message);
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Flush();
+
+                    Console.WriteLine("Sent message: " + message);
+                    clientSocket.Close();
+                }
+
+                serverSocket.Stop();
+                Console.WriteLine("Server stopped");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
